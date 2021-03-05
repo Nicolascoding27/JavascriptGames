7d72d7c31f2fe2f4ea2d62adf5fcf730d09b331e
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-03-05 15:18:14 -0500

    timer added

--- Guacamole/app.js
+++ Guacamole/app.js
@@ -1,9 +1,10 @@
 //first select all the div with square 
 const square=document.querySelectorAll('.square')
 const mole=document.querySelectorAll('.mole')
-const time=document.querySelectorAll('#time');
+const timeLeft=document.querySelectorAll('#time');
 const score=document.querySelectorAll('#score');
 let result=0;
+let currentTime=timeLeft.textContent
 function RandomSquare(){ //place the mole in a random square
     square.forEach(className=>{
         className.classList.remove('mole')
@@ -13,18 +14,30 @@ function RandomSquare(){ //place the mole in a random square
 
 //id for the random postion 
      hitPosition= randomPosition.id
-    }
+} 
     square.forEach(id=> {
     id.addEventListener('mouseup',()=>{
     if(id.id ===hitPosition){
-        result+=1
+        result= result + 1 
         score.textContent = result
+        hitPosition=null;
     }
 })
 })
+
 //we need to move the mole to different squares
 function MoveMole(){
-    let timerid=null 
-    timerId=setInterval(RandomSquare,1000)
+    let timerId= null;
+    timerId=setInterval(RandomSquare,500)
 }
 MoveMole()
+function countDown(){
+    currentTime--
+    timeLeft.textContent= currentTime
+    if(currentTime===0){
+        clearInterval(timerId)
+        alert(`GAME OVER! Your final score is ${result}`)
+    }
+}
+
+let timerId=setInterval(countDown,1000)
