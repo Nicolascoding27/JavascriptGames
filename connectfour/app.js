cab2ce130130bbc8076194fbb9de743d80e03712
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-03-08 19:37:03 -0500

    function player added

---
+++ connectfour/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',() =>{
+    const squares=document,querySelectorAll('.grid div')
+    const result=document.querySelector('#result')
+    const displayCurrentPlayer=document.querySelector('#current-player')
+
+})
