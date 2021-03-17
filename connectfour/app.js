ff273981ddb6de98093f90593649c0f21dc580bb
NICOLAS DIAZ <nicolas.diaz27@hotmail.com>
2021-03-17 15:19:09 -0500

    For loop and function added

--- connectfour/app.js
+++ connectfour/app.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded',() =>{
-    const squares=document,querySelectorAll('.grid div')
+    const squares=document,querySelectorAll('.grid div');
     const result=document.querySelector('#result')
     const displayCurrentPlayer=document.querySelector('#current-player')
+    let currentplayer=1//iniciamos el current player en 1
+    for(let i=0;i<squares.length;i++)
+    (function(index){
+        squares[i].onClick= function(){
+            
+        }
+
+
+
+    })(i)
 
 })
